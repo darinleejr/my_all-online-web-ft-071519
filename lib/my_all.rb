2f1f83ef9750ab8c78849dda0d0f406b96df3087
Darin Webster <darinleenavy@gmail.com>
2019-08-22 18:38:48 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,10 @@
-require 'pry'
-
 def my_all?(collection)
-
+  new_collection = []
+  x = 0 
+  while x < collection.each
+    puts x
+    break if x < 0 
+    new_collection << yield(x)
+    x += 1
+  end
 end
